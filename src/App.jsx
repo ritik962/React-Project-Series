b8b749da14b284c332bb7c99aceb2d1e24bd1e44
Ritik Roshan <ritikroshan5387744@gmail.com>
2025-01-12 10:52:31 +0530

    Login Signup Form Added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LoginSignupForm from "./components/LoginSignupForm/LoginSignupForm";
+
+const App = () => {
+  return (
+    <div>
+      <LoginSignupForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/LoginSignupForm/LoginSignupForm.jsx
@@ -0,0 +1,75 @@
+import "./loginSignup.css";
+import user_icon from "../Assets/person.png";
+import email_icon from "../Assets/email.png";
+import password_icon from "../Assets/password.png";
+import { useState } from "react";
+
+const LoginSignupForm = () => {
+  const [inputName, setInputName] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [action, setAction] = useState("Sign Up");
+  return (
+    <div className="wrapper">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="user-icon" />
+            <input
+              type="text"
+              placeholder="Name"
+              value={inputName}
+              onChange={(e) => setInputName(e.target.value)}
+            />
+          </div>
+        )}
+
+        <div className="input">
+          <img src={email_icon} alt="email-icon" />
+          <input
+            type="email"
+            placeholder="Email id"
+            value={inputEmail}
+            onChange={(e) => setInputEmail(e.target.value)}
+          />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="password-icon" />
+          <input
+            type="password"
+            name="new-password"
+            placeholder="Password"
+            value={inputPassword}
+            onChange={(e) => setInputPassword(e.target.value)}
+            autoComplete="new-password"
+          />
+        </div>
+        <p className="message">
+          Lost Password? <a href="#">Click here!</a>
+        </p>
+      </div>
+      <div className="action-btns">
+        <button
+          className={action === "Login" ? "btn signup gray" : "btn signup"}
+          onClick={() => setAction("Sign Up")}
+        >
+          sign up
+        </button>
+        <button
+          className={action === "Sign Up" ? "btn signin gray" : "btn signin"}
+          onClick={() => setAction("Login")}
+        >
+          sign in
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignupForm;
